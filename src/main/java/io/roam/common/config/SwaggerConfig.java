9030f64c11d064f39293b1382965360bd59f8578
오익준 <put_data@naver.com>
2025-05-12 19:37:31 +0900

    feat: Swagger 적용

---
+++ src/main/java/io/roam/common/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package io.roam.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Roamio API")
+                .description("Roam API 문서"));
+    }
+}
