c5024d7175c3037f2faee01cded87ac8136a8987
오익준 <put_data@naver.com>
2025-05-12 17:38:43 +0900

    refactor: User JPA형식으로 변경

--- src/main/java/io/roam/user/domain/User.java
+++ src/main/java/io/roam/user/domain/User.java
@@ -1,15 +1,31 @@
 package io.roam.user.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
+@Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private UserRole userRole;
 }
