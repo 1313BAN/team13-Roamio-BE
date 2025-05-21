83754df81d72506cd9b4c982fa9999cba23d638d
오익준 <put_data@naver.com>
2025-05-21 18:04:39 +0900

    chore: websocket spring boot app initialize

---
+++ roamio-websocket/src/test/java/io/roamio/websocket/WebSocketApplicationTests.java
@@ -0,0 +1,15 @@
+package io.roamio.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class WebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
