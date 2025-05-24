8c6851f44db98c89d2f9f4091940d23c698f7eb5
오익준 <put_data@naver.com>
2025-05-25 07:25:09 +0900

    fix: PlanRepository annotation 누락

--- roamio-domain/src/main/java/io/roam/plan/repository/PlanRepository.java
+++ roamio-domain/src/main/java/io/roam/plan/repository/PlanRepository.java
@@ -3,9 +3,10 @@ package io.roam.plan.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import io.roam.plan.entity.Plan;
 
+@Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
     List<Plan> findByOwnerId(Long ownerId);
 }
